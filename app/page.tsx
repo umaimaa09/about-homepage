a996f584ff2bb9ea8680ab93712a2c32dc7d54bc
umaimaa09 <161639086+umaimaa09@users.noreply.github.com>
2024-10-06 20:14:26 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import About from "./components/About";
+
+export default function Home() {
+  return (
+   <div> 
+<h1>This is a homepage</h1>
+<Header></Header>
+<About></About>
+   </div>
+   
+  
+  );
+};
